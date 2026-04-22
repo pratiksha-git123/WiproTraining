7d80b3ad34058edf94789869dbd1a80b592065bb
pratiksha-git123 <punyapratikshasahoo@gmail.com>
2026-04-22 22:11:48 +0530

    Python basics

---
+++ python/PythonCoding/daily_assignments/count_a_day1.py
@@ -0,0 +1,6 @@
+s = input("Enter a string: ")
+count = 0
+for index, char in enumerate(s):
+    if char == "a":
+        count += 1
+print("Total a appears: ", count)        
---
+++ python/PythonCoding/daily_assignments/sum_num_day1.py
@@ -0,0 +1,5 @@
+total = 0
+#1 to 50
+for i in range(1, 51):
+    total += i
+print(total)    
