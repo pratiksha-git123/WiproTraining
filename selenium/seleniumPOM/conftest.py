df2a424e310895d1dee64dabcda16724a6e0a100
pratiksha-git123 <punyapratikshasahoo@gmail.com>
2026-05-06 13:35:49 +0530

    Python basics

---
+++ selenium/seleniumPOM/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope='module')
+def driver():
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.get("https://www.amazon.in")
+    # time.sleep(5)
+    yield driver
+    driver.quit()
