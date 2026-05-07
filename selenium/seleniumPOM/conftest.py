bc3eaf87cd4f99991b2eb1fdbb20824f3ef57193
pratiksha-git123 <punyapratikshasahoo@gmail.com>
2026-05-08 00:01:03 +0530

    Python basics

--- selenium/seleniumPOM/conftest.py
+++ selenium/seleniumPOM/conftest.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from selenium import webdriver
 
@@ -6,7 +7,8 @@ from selenium import webdriver
 def driver():
     driver = webdriver.Edge()
     driver.maximize_window()
-    driver.get("https://www.amazon.in")
-    # time.sleep(5)
+    driver.get('https://www.amazon.in')
+    time.sleep(2)
+    # driver.find_element(By.XPATH,"//button[text()='Continue shopping']").click()
     yield driver
     driver.quit()
